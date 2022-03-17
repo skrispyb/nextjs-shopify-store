8b70678cfa8bda4519de55750bca33432c0d9f05
Sumeet Kumar <sumeet@techbytesfoundation.org>
2022-03-17 16:18:08 -0700

    adds next config file

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    loader: 'imgix',
+    path: 'https://skrispyb.github.io' || 'http://localhost:3000/',
+  },
+  reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+}
